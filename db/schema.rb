8820143d7eaa6d2457b6e45d84979806ff494b23
skipsuva <skip.suva@gmail.com>
2016-09-26 08:38:14 -0400

    change DB to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20160925181423) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "games", force: :cascade do |t|
     t.integer  "user_id"
     t.boolean  "player_won"
